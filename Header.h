b48ae7dd334acfa9f72001328f42df1e940b49c7
jeremiahulate <147085328+jeremiahulate@users.noreply.github.com>
2025-08-07 22:36:53 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,7 +2,7 @@
 void ascendingTriangle() {
     for (int i = 1; i <= 6; i++) {
         for (int j = 1; j <= i; j++) {
-            if ((i == 1 && j <= 1) || (i == 2 && j <= 2) || (i == 3 && j <= 3) || (i == 4 && j <= 4) || (i == 5 && j <= 5) || (i == 6 && j <= 6)) {
+            if((1<i<6) && (j <=i)) {
                 printf("%d", i);
             } else {
                 printf(" ");
@@ -17,7 +17,7 @@ void ascendingTriangle() {
 void descendingTriangle() {
     for (int i = 6; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
-            if ((i == 1 && j <= 1) || (i == 2 && j <= 2) || (i == 3 && j <= 3) || (i == 4 && j <= 4) || (i == 5 && j <= 5) || (i == 6 && j <= 6)) {
+            if((1<i<6) && (j <=i)) {
                 printf("%d", i);
             } else {
                 printf(" ");
